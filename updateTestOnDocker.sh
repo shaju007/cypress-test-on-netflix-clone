13fb96d23abf757b83d1c40b232bd5e7f6e53736
Shaju <shaju007@users.noreply.github.com>
2021-12-05 14:51:12 +0100

    Update updateTestOnDocker.sh

--- updateTestOnDocker.sh
+++ updateTestOnDocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm i & npm run start & docker run -i -e CYPRESS_updateSnapshots=true -e CYPRESS_baseUrl=http://host.docker.internal:8080 -v /$PWD:$PWD -w $PWD cypress/included:8.7.0
+export CYPRESS_baseUrl="http://$(ip route | grep -E '(default|docker0)' | grep -Eo '([0-9]+\.){3}[0-9]+' | tail -1):8080" & npm i & npm run start & docker run -i -e CYPRESS_updateSnapshots=true -e CYPRESS_baseUrl=$CYPRESS_baseUrl -v /$PWD:$PWD -w $PWD cypress/included:8.7.0
