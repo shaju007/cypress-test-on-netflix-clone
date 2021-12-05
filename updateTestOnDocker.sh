68b4a194c41aba329b5238918fb2fe17e625f300
Shaju <shaju007@users.noreply.github.com>
2021-12-05 17:48:29 +0100

    Update updateTestOnDocker.sh

--- updateTestOnDocker.sh
+++ updateTestOnDocker.sh
@@ -1,1 +1,4 @@
+npm install
+export CYPRESS_VIDEO=true
+export CYPRESS_baseUrl="http://$(ip route | grep -E '(default|docker0)' | grep -Eo '([0-9]+\.){3}[0-9]+' | tail -1):8080"
 npm run start & docker run -i -e CYPRESS_VIDEO -e CYPRESS_updateSnapshots=true -e CYPRESS_baseUrl=$CYPRESS_baseUrl -v /$PWD:$PWD -w $PWD cypress/included:8.7.0
