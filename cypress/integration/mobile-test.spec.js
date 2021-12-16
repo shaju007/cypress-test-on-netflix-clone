68c4d0517aee875a5e35f8cfdf98c2218418549f
Shaju <shaju007@users.noreply.github.com>
2021-12-17 00:21:52 +0100

    New: custom command

--- cypress/integration/mobile-test.spec.js
+++ cypress/integration/mobile-test.spec.js
@@ -5,6 +5,12 @@ describe('test footer section', function () {
     it('test footer', function () {
       cy.visit('/');
       cy.get('.btn-xl').should('have.css', 'font-size', '16px');
+      cy.clickTab(1).then(() => {
+          cy.get('.tab-1-content-inner p').should('contain', 'If you decide')
+      })
+      cy.clickTab(2).then(() => {
+        cy.get('.tab-2-content-top p').should('contain', 'Watch TV shows and')
+    })
     });
   });
   
