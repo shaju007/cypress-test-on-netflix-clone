dd66c10f974818cab54db4e8ac8bdb3b8f347560
Shaju <shaju007@users.noreply.github.com>
2021-12-16 23:31:46 +0100

    Update: Check Font Size

--- cypress/integration/mobile-test.spec.js
+++ cypress/integration/mobile-test.spec.js
@@ -4,7 +4,7 @@ describe('test footer section', function () {
     })
     it('test footer', function () {
       cy.visit('/');
-      cy.get('.footer .footer-cols').should('have.css', 'grid-template-columns', '126.25px 126.25px');
+      cy.get('.btn-xl').should('have.css', 'font-size', '16px');
     });
   });
   
